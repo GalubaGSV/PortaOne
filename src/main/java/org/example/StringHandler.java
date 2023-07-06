4b97ce169a551cf491a28471aedf68e1a491bfeb
Sergiy Golubchenko <galuba1@gmail.com>
2023-07-06 17:25:02 +0300

    Add constants

--- src/main/java/org/example/StringHandler.java
+++ src/main/java/org/example/StringHandler.java
@@ -4,8 +4,11 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class StringHandler {
+    private final static String WORD_SEPARATE = "\\s+";
+    private final static char DEFAULT_RETURN_SYMBOL = '\0';
+
     public char findMagicSymbol(String string) {
-        String[] words = string.trim().trim().split("\\s+");
+        String[] words = string.trim().trim().split(WORD_SEPARATE);
         StringBuilder stringBuilder = new StringBuilder();
         for (String str : words) {
             stringBuilder.append(findUniqueSymbolInWord(str));
@@ -23,6 +26,6 @@ public class StringHandler {
                 return entry.getKey();
             }
         }
-        return '\0';
+        return DEFAULT_RETURN_SYMBOL;
     }
 }
